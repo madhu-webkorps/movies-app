58a503ac09db40fa52d64ae433121ad81cc19b24
madhu-webkorps <madhu@webkorps.com>
2022-11-16 11:51:22 +0530

    validations for movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,17 @@ class Movie < ApplicationRecord
     has_many :movie_with_users ,dependent: :destroy
     has_many :users, through: :movie_users 
     # has_many :genres
+
+# validations
+ validates   :name, presence: true, 
+             length:{ maximum: 80 }
+
+ year  format: { with: /\A\d{4}\z/ ,
+      message: "only allows digits" }
+
+ director    length: { maximum: 60 }
+
+ main_star    length:  { maximum: 60 }
+ 
+ discription  length:  { maximum: 400 }
 end
